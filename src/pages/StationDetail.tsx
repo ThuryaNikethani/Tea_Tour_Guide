aeec6bf0bf2ba1d768997ddac7654b76f5203f6c
Thinu <208908942+ThuryaNikethani@users.noreply.github.com>
2026-09-07 10:14:50 +0530

    Left-align a single bottom-placed contain image instead of centering it

--- src/pages/StationDetail.tsx
+++ src/pages/StationDetail.tsx
@@ -238,7 +238,7 @@ function SectionContent({ section }: { section: StationSection }) {
         ))}
       </div>
     ) : (
-      <img src={section.image} alt={section.heading} loading="lazy" className={`${imageClass} mb-3 ${isContain ? "mx-auto max-w-full" : ""}`} />
+      <img src={section.image} alt={section.heading} loading="lazy" className={`${imageClass} mb-3 ${isContain ? "max-w-full" : ""}`} />
     )
   );
 
